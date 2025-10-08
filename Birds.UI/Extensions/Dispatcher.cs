4ba99b7e55a65e417e08afb0711794d8bde1b34f
Londongrad <teslagradus@gmail.com>
2025-10-08 23:08:19 +0900

    feat(dispatcher): add convenient method to check UI thread and update UI collections

---
+++ Birds.UI/Extensions/Dispatcher.cs
@@ -0,0 +1,13 @@
+﻿namespace Birds.UI.Extensions
+{
+    public static class Dispatcher
+    {
+        public static async Task InvokeOnUiAsync(this System.Windows.Threading.Dispatcher dispatcher, Action action)
+        {
+            if (dispatcher.CheckAccess())
+                action();
+            else
+                await dispatcher.InvokeAsync(action);
+        }
+    }
+}
