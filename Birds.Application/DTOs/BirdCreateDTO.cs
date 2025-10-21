4a1fa80d83cb2a23f1f310f311ef891e27cdf80e
Londongrad <Teslagradus@gmail.com>
2025-10-21 21:52:29 +0900

    feat(create-dto): add Departure and IsAlive properties to support creating one-time birds

--- Birds.Application/DTOs/BirdCreateDTO.cs
+++ Birds.Application/DTOs/BirdCreateDTO.cs
@@ -6,5 +6,11 @@ namespace Birds.Application.DTOs
     /// Data transfer object used to create a new bird.
     /// Contains only fields required for creation.
     /// </summary>
-    public record BirdCreateDTO(BirdsName Name, string? Description, DateOnly Arrival);
+    public record BirdCreateDTO(
+        BirdsName Name,
+        string? Description,
+        DateOnly Arrival,
+        DateOnly? Departure = null,
+        bool IsAlive = true
+    );
 }
