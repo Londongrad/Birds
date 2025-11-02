34105c4ec78aa631fc4ec9aaa9e92e8bd1e7e672
Londongrad <Teslagradus@gmail.com>
2025-11-02 18:24:16 +0900

    feat(notification): add INotificationManager interface

---
+++ Birds.UI/Services/Notification/Interfaces/INotificationManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Birds.UI.Services.Notification.Interfaces
+{
+    public interface INotificationManager
+    {
+        void ShowNotification(string message, NotificationOptions options, Window parent);
+    }
+}
