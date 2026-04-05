c611a05799903819508e5a95b11dfdde68e30db2
Londongrad <teslagradus@gmail.com>
2026-04-05 11:07:00 +0900

    style(main-window): add custom dark title bar

--- Birds.UI/Views/Windows/MainWindow.xaml.cs
+++ Birds.UI/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Input;
 
 namespace Birds.UI.Views.Windows
 {
@@ -7,6 +8,57 @@ namespace Birds.UI.Views.Windows
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += (_, _) => UpdateWindowStateGlyph();
+            StateChanged += (_, _) => UpdateWindowStateGlyph();
+        }
+
+        private void OnTitleBarMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && ResizeMode == ResizeMode.CanResize)
+            {
+                ToggleWindowState();
+                return;
+            }
+
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void OnMinimizeClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void OnMaximizeRestoreClick(object sender, RoutedEventArgs e)
+        {
+            ToggleWindowState();
+        }
+
+        private void OnCloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ToggleWindowState()
+        {
+            WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
+
+        private void UpdateWindowStateGlyph()
+        {
+            if (MaximizeRestoreGlyph == null)
+            {
+                return;
+            }
+
+            MaximizeRestoreGlyph.Text = WindowState == WindowState.Maximized
+                ? "❐"
+                : "□";
         }
     }
 }
